cff82be1abb9fd1b45a9294e7289483bbcadf4c7
Bruno4745 <bruno_4745@hotmail.com>
2021-04-08 14:51:02 -0300

    Consultando dados da API

---
+++ js/functions.js
@@ -0,0 +1,23 @@
+let url = "https://frontend-intern-challenge-api.iurykrieger.vercel.app/products?page=1";
+
+//Get data from the API
+async function getProducts() {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+        
+        //Browse all products on a page
+        // for(let i=0; i<data.products.length; i++){
+        //     console.log(data.products[i]);
+        // }
+
+        //Defines the next API page
+        url = `https://${data.nextPage}`;
+
+        return data;
+
+    } catch(erro) {
+        console.log("[ERROR] " + erro);
+    }
+}
