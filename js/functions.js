9ff1136ff1e80b5ea82fa54d57a2fdf106cd6392
Bruno4745 <bruno_4745@hotmail.com>
2021-04-11 00:13:11 -0300

    Inserindo link nos botões e validação nos formulários

--- js/functions.js
+++ js/functions.js
@@ -18,9 +18,9 @@ async function getProducts() {
                                 <div class="card-container">
                                     <p class="text14px textName">${data.products[i].name}</p>
                                     <p class="text12px textDescription">${data.products[i].description}</p>
-                                    <p class="text12px textOldPrice">De: R$${data.products[i].oldPrice}</p>
-                                    <p class="text16px textPrice"><b>Por: R$${data.products[i].price}</b></p>
-                                    <p class="text12px textInstallments">ou ${data.products[i].installments.count}x de R$${data.products[i].installments.value}</p>
+                                    <p class="text12px textOldPrice">De: R$${data.products[i].oldPrice.toFixed(2).replace(".",",")}</p>
+                                    <p class="text16px textPrice"><b>Por: R$${data.products[i].price.toFixed(2).replace(".",",")}</b></p>
+                                    <p class="text12px textInstallments">ou ${data.products[i].installments.count}x de R$${data.products[i].installments.value.toFixed(2).replace(".",",")}</p>
                                     <button class="button-style card-button">Comprar</button>
                                 </div>
                             </div>`;
@@ -39,3 +39,24 @@ async function getProducts() {
 
 // Calls the function to load the products from the first page of the API
 getProducts();
+
+function checkEmail(value, id) {
+    //console.log(value.indexOf('.com'));
+    if(value.indexOf('@') == -1 || value.indexOf('.com') == -1  || value.indexOf('.com') < value.indexOf('@') || (value.indexOf('.com') - value.indexOf('@')) == 1) {
+	    alert("Por favor, informe um e-mail válido!");
+        document.getElementById(id).value = "";
+	    return false;
+	}
+}
+
+function sendForm() {
+    alert("Formulário enviado!");
+}
+
+function checkName(value, id) {
+    // Removes all spaces
+    if(value.trim().length == 0){
+        alert("Por favor, informe um nome válido!");
+        document.getElementById(id).value = "";
+    }
+}
